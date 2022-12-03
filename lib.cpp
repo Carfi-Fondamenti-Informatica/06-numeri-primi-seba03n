ec100196435396c48b7cb6db043bf94fa7003ab8
seba03n <115528575+seba03n@users.noreply.github.com>
2022-12-03 09:11:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,12 +1,12 @@
 #include "lib.h"
-bool n_primo (int a, int b){
-  if (a % i == 0){
-  if (a == i){
-    return true;
-  }
-    else{
-      return false;
+bool numprimo(int a, int i){
+    if(a % i == 0){
+        if(a == i){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-  }
-  n_primo (a , i + 1);
+    numprimo(a, i + 1);
 }
